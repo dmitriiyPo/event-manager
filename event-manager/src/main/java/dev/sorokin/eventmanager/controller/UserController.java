6a055794cc23bce5ae3062af679d258362ff0a97
Dmitriy Ponomarev <ponomarev.dmitriiy@gmail.com>
2026-05-05 11:38:32 +0600

    [*] little change controller, ResponseEntity, body

--- event-manager/src/main/java/dev/sorokin/eventmanager/controller/UserController.java
+++ event-manager/src/main/java/dev/sorokin/eventmanager/controller/UserController.java
@@ -5,7 +5,6 @@ import dev.sorokin.eventmanager.dto.user.request.SignInUserRequest;
 import dev.sorokin.eventmanager.dto.user.request.SignUpUserRequest;
 import dev.sorokin.eventmanager.security.jwt.JwtTokenResponse;
 import dev.sorokin.eventmanager.dto.user.response.UserDtoResponse;
-import dev.sorokin.eventmanager.model.User;
 import dev.sorokin.eventmanager.security.jwt.AuthenticationService;
 import dev.sorokin.eventmanager.service.UserService;
 import io.swagger.v3.oas.annotations.security.SecurityRequirements;
@@ -34,22 +33,25 @@ public class UserController {
     @SecurityRequirements()
     @PostMapping
     public ResponseEntity<UserDtoResponse> registerUser(@RequestBody @Valid SignUpUserRequest signUpRequest) {
-        User user = userService.registerUser(signUpRequest);
-        return ResponseEntity.status(HttpStatus.CREATED).body(userDtoConverter.toDto(user));
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(userDtoConverter.toDto(userService.registerUser(signUpRequest)));
     }
 
     @SecurityRequirements()
     @PostMapping("/auth")
     public ResponseEntity<JwtTokenResponse> authenticate(@RequestBody @Valid SignInUserRequest signInRequest) {
-        String token = authenticationService.authenticateUser(signInRequest);
-        return ResponseEntity.status(HttpStatus.OK).body(new JwtTokenResponse(token));
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(new JwtTokenResponse(authenticationService.authenticateUser(signInRequest)));
     }
 
 
     @GetMapping("/{userId}")
     public ResponseEntity<UserDtoResponse> findUserById(@PathVariable("userId") Long userId) {
-        User foundUser = userService.findUserById(userId);
-        return ResponseEntity.status(HttpStatus.OK).body(userDtoConverter.toDto(foundUser));
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(userDtoConverter.toDto(userService.findUserById(userId)));
     }
 
 }
